13cfcc5e4c420cb55c920cf6d7fb7dca2a52d2c5
PuputPakaya <85119347+PuputPakaya@users.noreply.github.com>
2021-06-16 12:30:37 +0530

    Update latihan.js

--- latihan.js
+++ latihan.js
@@ -9,10 +9,10 @@ alert(tes +nama);
    
 
 if ( tes === true) {
-    alert('anda menyetujui!')
+    alert('nama dikonfirmasi!')
 }
     else{
-    alert('anda membatalkan!')
+    alert('nama tidak dikonfirmasi!')
 }
 
 // latihan confirm while
